c60c26086a108fc000d9bc010ae44074e8983ade
3A532055 <ylhc214044@gmail.com>
2018-10-09 17:33:45 +0800

    類別自動載入Lara

---
+++ src/Demo/Hello/Lara.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/10/9
+ * Time: 下午 05:31
+ */
+
+namespace Demo\Hello;
+
+
+class Lara
+{
+    function __construct()
+    {
+        echo 'Hello Lara~'.PHP_EOL;
+    }
+
+}
